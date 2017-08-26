f156b3b0d4a88dce547be614501c8715529dc444
Benjamin Conway <benrconway84@gmail.com>
2017-08-26 20:55:32 +0100

    Code view for Animal CRUD

--- controllers/animal_controller.rb
+++ controllers/animal_controller.rb
@@ -4,7 +4,24 @@ require_relative("../models/animal.rb")
 require_relative("../models/member.rb")
 require_relative("../models/sponsorship.rb")
 
-
+#Index route
 get "/animals" do
-  erb(:"")
+  @animals = Animal.all()
+  erb(:"animals/index")
+end
+
+# Create route
+get "/animals/new" do
+  erb(:"animals/new")
+end
+
+
+get "/animals/:id/edit" do
+  @animal = Animal.find(params[:id])
+  erb(:"animals/edit")
+end
+
+get "/animals/:id" do
+  @animal = Animal.find(params[:id])
+  erb(:"animals/show")
 end
