de41e05ad0c11d6ae6a6455f29fa62766a7aea12
Benjamin Conway <benrconway84@gmail.com>
2017-08-27 18:12:37 +0100

    Sponsorship edit drop down

--- controllers/sponsorship_controller.rb
+++ controllers/sponsorship_controller.rb
@@ -3,3 +3,49 @@ require("sinatra/contrib/all")
 require_relative("../models/animal.rb")
 require_relative("../models/member.rb")
 require_relative("../models/sponsorship.rb")
+
+#Index
+get "/sponsorships" do
+  @s_ships = Sponsorship.all()
+  erb(:"sponsorships/index")
+end
+
+
+
+#edit
+get "/sponsorships/:id/edit" do
+  @animals = Animal.all()
+  @members = Member.all()
+  @record = Sponsorship.find(params[:id])
+  erb(:"sponsorships/edit")
+end
+
+post "/sponsorships/:id" do
+  record = Sponsorship.new(params)
+  record.update()
+  redirect to "/sponsorships/#{record.id}"
+end
+#new
+get "/sponsorships/new" do
+  @animals = Animal.all()
+  @members = Member.all()
+  erb(:"sponsorships/new")
+end
+
+post "/sponsorships" do
+  record = Sponsorship.new(params)
+  record.save()
+  redirect to "/sponsorships"
+end
+
+#delete
+post "/sponsorships/:id/delete" do
+  Sponsorship.delete_by_id(params[:id])
+  redirect to "/sponsorships"
+end
+
+#Show
+get "/sponsorships/:id" do
+  @record = Sponsorship.find(params[:id])
+  erb(:"sponsorships/show")
+end
